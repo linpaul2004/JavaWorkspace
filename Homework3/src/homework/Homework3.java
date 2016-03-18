f33bc351797759fc4c433fabba7dacc085af2080
linpaul2004 <linpaul2004@gmail.com>
2016-03-18 19:07:37 +0800

    Homework3
    Improved Method of Exhaustion

--- Homework3/src/homework/Homework3.java
+++ Homework3/src/homework/Homework3.java
@@ -1,13 +1,45 @@
 package homework;
 
+import java.util.HashSet;
+
 public class Homework3 {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-		String[] input = args[0].split("\n");
-		for (int i = 0; i < input.length; i++) {
-			if (input[i].charAt(0) == 'X') {
-				System.out.println(check(Integer.parseInt(input[i].substring(2))));
+		if (args[0].charAt(0) == 'X' || args[0].charAt(0) == 'x') {
+			System.out.println(check(Integer.parseInt(args[0].substring(2))));
+		} else {
+			int index = Integer.parseInt(args[0].substring(2));
+			if (index <= 6) {
+				System.out.println(index);
+			} else {
+				HashSet<Integer> sequence = new HashSet<>();
+				sequence.add(1);
+				sequence.add(2);
+				sequence.add(3);
+				sequence.add(4);
+				sequence.add(5);
+				sequence.add(6);
+				int i;
+				for (i = 7; sequence.size() != index; i++) {
+					int tmp = i;
+					while (true) {
+						if (tmp % 2 == 0) {
+							tmp /= 2;
+						} else if (tmp % 3 == 0) {
+							tmp /= 3;
+						} else if (tmp % 5 == 0) {
+							tmp /= 5;
+						} else {
+							break;
+						}
+						if (sequence.contains(tmp)) {
+							sequence.add(i);
+							break;
+						}
+					}
+				}
+				System.out.println(i - 1);
 			}
 		}
 	}
@@ -22,7 +54,7 @@ public class Homework3 {
 		while (n % 5 == 0) {
 			n /= 5;
 		}
-		return n == 0 ? true : false;
+		return n == 1 ? true : false;
 	}
 
 }
