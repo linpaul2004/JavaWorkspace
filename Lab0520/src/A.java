deb4611487f590b074e56eb7fda964243c08411a
linpaul2004 <linpaul2004@gmail.com>
2016-05-20 17:44:59 +0800

    Java Class 0520

---
+++ Lab0520/src/A.java
@@ -0,0 +1,9 @@
+
+public class A extends B implements E {
+	D d = new D();
+
+	public void m2() {
+		C c = new C();
+		c.m1();
+	}
+}
---
+++ Lab0520/src/tr/Country.java
@@ -0,0 +1,5 @@
+package tr;
+
+public class Country {
+	City capital = new City();
+}
